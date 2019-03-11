46e7576d6bb0a13d8fc4163ee2b34524ae564b66
vanguard <vanguardd@outlook.com>
2019-03-11 17:34:59 +0800

    修改一些依赖

--- src/main/java/com/team/seahouse/SeaHouseApplication.java
+++ src/main/java/com/team/seahouse/SeaHouseApplication.java
@@ -2,6 +2,8 @@ package com.team.seahouse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import tk.mybatis.spring.annotation.MapperScan;
 
 /**
@@ -13,9 +15,14 @@ import tk.mybatis.spring.annotation.MapperScan;
  */
 @SpringBootApplication
 @MapperScan(basePackages = "com.team.seahouse.mapper")
-public class SeaHouseApplication {
+public class SeaHouseApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SeaHouseApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(this.getClass());
 	}
 }
